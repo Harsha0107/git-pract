5feeab5e79a28dc2fae250622f7c93658775af84
Harsha0107 <hv99891@gmail.com>
2025-09-20 14:13:18 +0530

    variables

---
+++ 06-ARRAY.sh
@@ -0,0 +1,7 @@
+# This is an array declaration in bash
+FRUITS=("apple" "banana" "cherry")
+
+# Accessing array elements
+echo "First fruit: ${FRUITS[0]}"
+echo "Second fruit: ${FRUITS[1]}"
+echo "Third fruit: ${FRUITS[2]}"
