b9249212ba3749c3269effb776f566a33f77684d
Harsha0107 <hv99891@gmail.com>
2025-09-20 13:54:40 +0530

    added variables arguments script

---
+++ 04-VARIABLES.sh
@@ -0,0 +1,17 @@
+PERSON1=$1
+PERSON2=$2
+ 
+ 
+echo "$PERSON1" : hi "$PERSON2" how are you?"
+echo "$PERSON2" : hi "$PERSON1" I am fine and you?"
+echo "$PERSON1" : I am also fine.
+echo "$PERSON2" : Great to hear that.
+echo "$PERSON1" : What are you doing these days?
+echo "$PERSON2" : I am learning DevOps.
+echo "$PERSON1" : That's awesome! DevOps is a great field.
+
+
+# End of the script
+# This script demonstrates the use of variables in bash scripting
+# It defines two variables PERSON1 and PERSON2 and uses them in echo statements to simulate a conversation.
+
