24fff1328ecb8684540ad5dded71854b91d2c01e
Harsha0107 <hv99891@gmail.com>
2025-09-20 14:02:58 +0530

    added variables arguments/input script

--- 05-VARIABLES.sh
+++ 05-VARIABLES.sh
@@ -1,3 +1,16 @@
 echo "please ENTER your username"
-read USERNAME
+read -s USERNAME
 echo "username is : $USERNAME"
+
+
+echo "please ENTER your password"
+read -s PASSWORD
+echo "password is : $PASSWORD"
+
+echo "please ENTER your email"
+read -s EMAIL
+echo "email is : $EMAIL"
+
+echo "please ENTER your phone number"
+read -s PHONE
+echo "phone number is : $PHONE"
