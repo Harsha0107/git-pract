b0ed4ac6af8094972ee75318311d2e131762f82a
Harsha0107 <hv99891@gmail.com>
2025-09-22 22:13:11 +0530

    commands-output

--- 11-installation.sh
+++ 11-installation.sh
@@ -2,4 +2,11 @@
 
 USERID=$(id -u)
 
-echo "User ID is $USERID"
+#echo "User ID is $USERID"
+if [ $USERID -ne 0 ]
+then
+  echo "Please run as root user"
+fi
+
+dnf install git -y
+echo "Git installed successfully"
