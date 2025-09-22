f5de4d59b378aa8a4a4832b588d43010417d40d1
Harsha0107 <hv99891@gmail.com>
2025-09-22 22:26:25 +0530

    commands-output

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#USERID=$(id -u)
-#echo "User ID is $USERID"
-#if [ $USERID -ne 0 ]
-#then 
- # echo "Please run as root user"
-#fi
+USERID=$(id -u)
+echo "User ID is $USERID"
+if [ $USERID -ne 0 ]
+then 
+echo "Please run as root user"
+exit 1
+fi
 
 
-ls -ltr 
-ls -l
-ls -la 
