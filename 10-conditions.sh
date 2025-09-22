3fcf9f1c1208fa9702e7547e7e2fc156c6685104
Harsha0107 <hv99891@gmail.com>
2025-09-22 21:50:28 +0530

    commands-output

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # if and else conditions in bash scripting
 NUMBER=$1
-if [ $NUMBER -gt  20 ]
+if [ $NUMBER -gt 20 ]
 then 
   echo "$NUMBER is greater than 20"
 else
-    echo "$NUMBER is less than or equal to 20"
+    echo "$NUMBER is less than 20"
 fi
