4004b870a42a6d2a19c757b44c702b021ececcb8
Harsha0107 <hv99891@gmail.com>
2025-09-22 22:39:07 +0530

    commands-output

--- 11-installation.sh
+++ 11-installation.sh
@@ -24,3 +24,20 @@ then
 else
         echo "Git is already installed"
 fi
+
+
+dnf list installed mysql -y
+if [ $? -ne 0 ]
+then
+echo "mysql is not installed"
+dnf install mysql -y
+if [ $? -ne 0 ]
+then
+echo "mysql installation is not success..."
+exit 1
+else
+echo "mysql installation is successful"
+fi
+else
+echo "mysql is already installed"
+fi
