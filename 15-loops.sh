716df951be2a723590bbf04f00c09e2606333adf
Harsha0107 <hv99891@gmail.com>
2025-09-28 09:36:30 +0530

    for loops

---
+++ 15-loops.sh
@@ -0,0 +1,37 @@
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+C="\e[36m"
+W="\e[37m"
+
+CHECK_ROOT(){
+    if [USERID -ne 0]
+    then 
+        echo "please un this script as root privileges"
+        exit 1
+    fi 
+}
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then 
+        echo -e "$2 is ....$R FAILED $M"
+        exit 1
+    else
+        echo -e "$2 is ....$G SUCCESS $M"
+    fi
+} 
+
+
+CHECK_ROOT
+
+#sh 15-loops.sh git mysql postfix nginx docker httpd vsftpd ssh 
+
+for package in $@ #$@ is special variable which takes all the arguments passed to the script
+do 
+   echo  $package
+   
+done
