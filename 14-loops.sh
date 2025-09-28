33be906c4972a92736c3e6fb3bcd25cb3c98f35c
Harsha0107 <hv99891@gmail.com>
2025-09-28 09:27:32 +0530

    for loops

---
+++ 14-loops.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Loops
+# for loop
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+# echo "7"
+# echo "8"
+# echo "9"
+# echo "10"
+
+
+
+for i in {0..100}
+do 
+echo $i
+done
